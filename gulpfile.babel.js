1b7dfafc4dc644ee54a8aa327dc16090822f1158
Alexandr <caiiiia_podoplelov@mail.ru>
2021-11-11 23:47:30 +0300

    Builded pug files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,13 @@
 import gulp from "gulp";
+import gPug from "gulp-pug";
+
+const routes = {
+  pug: {
+    src: 'src/*.pug',
+    dest: 'build'
+  }
+};
+
+export const pug = () => gulp.src(routes.pug.src).pipe(gPug()).pipe(gulp.dest(routes.pug.dest));
+
+export const dev = gulp.series([pug]);
