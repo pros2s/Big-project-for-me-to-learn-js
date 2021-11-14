eef8e9079cc8d890de2f33981b280c949bb688b8
Alexandr <caiiiia_podoplelov@mail.ru>
2021-11-14 12:15:12 +0300

    Watch PUG

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,23 +1,43 @@
 import gulp from "gulp";
 import gPug from "gulp-pug";
 import del from "del";
+import wServer from "gulp-webserver";
+import img from "gulp-image";
 
 const routes = {
   pug: {
+    watch: 'src/**/*.pug',
     src: 'src/*.pug',
     dest: 'build'
   }
 };
 
+//PUG
 const pug = () =>
   gulp.src(routes.pug.src)
   .pipe(gPug())
   .pipe(gulp.dest(routes.pug.dest));
 
+//Watch PUG
+const watchPug = () => {
+  gulp.watch(routes.pug.watch, pug);
+};
+
+//Clean
 const clean = () =>
   del(["build"]);
 
+//LifeReload
+const ws = () =>
+  gulp.src('build')
+  .pipe(wServer({
+    port: 7355,
+    livereload: true,
+    open: true
+  }));
+
 const prepare = gulp.series([clean]);
 const assets = gulp.series([pug]);
+const postDev = gulp.parallel([ws, watchPug]);
 
-export const dev = gulp.series([prepare, assets]);
+export const dev = gulp.series([prepare, assets, postDev]);
