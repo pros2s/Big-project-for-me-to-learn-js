407cee18603f2d4ec36fb30ddcc0ee7006bab7bf
Alexandr <caiiiia_podoplelov@mail.ru>
2021-11-14 12:26:49 +0300

    Optimizate and watch images

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,13 +2,17 @@ import gulp from "gulp";
 import gPug from "gulp-pug";
 import del from "del";
 import wServer from "gulp-webserver";
-import img from "gulp-image";
+import image from "gulp-image";
 
 const routes = {
   pug: {
     watch: 'src/**/*.pug',
     src: 'src/*.pug',
     dest: 'build'
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img"
   }
 };
 
@@ -18,9 +22,10 @@ const pug = () =>
   .pipe(gPug())
   .pipe(gulp.dest(routes.pug.dest));
 
-//Watch PUG
+//Watch
 const watchPug = () => {
   gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
 };
 
 //Clean
@@ -36,7 +41,13 @@ const ws = () =>
     open: true
   }));
 
-const prepare = gulp.series([clean]);
+//Images
+const img = () =>
+  gulp.src(routes.img.src)
+  .pipe(image())
+  .pipe(gulp.dest(routes.img.dest));
+
+const prepare = gulp.series([clean, img]);
 const assets = gulp.series([pug]);
 const postDev = gulp.parallel([ws, watchPug]);
 
