f1b54825eec660ec64459d447bef1ce7d06b70b1
Alexandr <caiiiia_podoplelov@mail.ru>
2021-11-16 15:54:29 +0300

    edit ajax for myself

--- src/js/ajaxModal.js
+++ src/js/ajaxModal.js
@@ -1,44 +1,64 @@
 window.addEventListener('DOMContentLoaded', function() {
-  /////////////////////////////////////////////////////////////////////////////
-  //form's data with backend
   const forms = document.querySelectorAll('form');
 
+  //List of stages' messages
   const message = {
-    loading: "Загрузка...",
-    success: "Спасибо, скоро мы с вами свяжемся",
-    failure: "Ошибка"
+    loading: 'loading',
+    succes: 'thanks, everything is good',
+    failure: 'Error'
   };
 
-  forms.forEach(item => {
-    postData(item);
-  });
+  const postData = (form) => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
 
-  function postData(form) {
-    form.addEventListener('submit', event => {
-      event.preventDefault();
-
-      let statusMessage = document.createElement('div');
+      //Create visuality of textmessages
+      const statusMessage = document.createElement('div');
       statusMessage.classList.add('status');
       statusMessage.textContent = message.loading;
       form.append(statusMessage);
 
+      //Create new object XMLHttpRequest()
       const request = new XMLHttpRequest();
       request.open('POST', 'php/server.php');
 
+      ///////////////////////////////////////////////////////////////////
+      // doesn't work with FormData()
       // request.setRequestHeader('Content-type', 'multipart/form-data');
+      ///////////////////////////////////////////////////////////////////
+
+      //Formdata of object
       const formData = new FormData(form);
 
-      request.send(formData);
+      //Data in json format
+      const object = {};
+      formData.forEach((value, key) => {
+        object[key] = value;
+      });
 
+      const json = JSON.stringify(object);
+      request.send(json /*or formData*/);
+
+      //Output response and message on 'load'
       request.addEventListener('load', () => {
-        if(request.status === 200) {
+        if (request.status === 200) {
           console.log(request.response);
           statusMessage.textContent = message.succes;
+          form.reset();
+
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 2000);
         }
         else {
           statusMessage.textContent = message.failure;
         }
       });
     });
-  }
+  };
+
+  //Do postData() for each form
+  forms.forEach(item => {
+    postData(item);
+  });
 });
