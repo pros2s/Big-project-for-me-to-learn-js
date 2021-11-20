b2d8cbb539838be91de6d1f44c962220ad7b115a
Alexandr <caiiiia_podoplelov@mail.ru>
2021-11-20 18:42:32 +0300

    show beautyfull modal succes

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
   const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal');
 
   modalTrigger.forEach(btn => {
     btn.addEventListener('click', openModal);
@@ -21,10 +20,8 @@ window.addEventListener('DOMContentLoaded', function() {
     document.body.style.overflow = '';
   }
 
-  modalCloseBtn.addEventListener('click', closeModal);
-
   modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
+    if (e.target === modal || e.target.getAttribute('data-close') == '') {
       closeModal();
     }
   });
@@ -35,7 +32,7 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 3000);
+  const modalTimerId = setTimeout(openModal, 30000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -45,4 +42,91 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   window.addEventListener('scroll', showModalByScroll);
+
+  /////////////////////////////////////////////////////////////////////////////////////
+  //ajax MODAL                                                                       //
+  /////////////////////////////////////////////////////////////////////////////////////
+  const forms = document.querySelectorAll('form');
+
+  //List of stages' messages
+  const message = {
+    loading: 'loading',
+    succes: 'thanks, everything is good',
+    failure: 'Error'
+  };
+
+  const postData = (form) => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+
+      //Create visuality of textmessages
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      //Create new object XMLHttpRequest()
+      const request = new XMLHttpRequest();
+      request.open('POST', 'php/server.php');
+
+      ///////////////////////////////////////////////////////////////////
+      // doesn't work with FormData()
+      // request.setRequestHeader('Content-type', 'multipart/form-data');
+      ///////////////////////////////////////////////////////////////////
+
+      //Formdata of object
+      const formData = new FormData(form);
+
+      //Data in json format
+      const object = {};
+      formData.forEach((value, key) => {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+      request.send(json /*or formData*/);
+
+      //Output response and message on 'load'
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          showThanksModal(message.succes);
+          form.reset();
+          statusMessage.remove();
+        }
+        else {
+          showThanksModal(message.failure);
+        }
+      });
+    });
+  };
+
+  function showThanksModal (message) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal__close" data-close>×</div>
+        <div class="modal__title">${message}</div>
+      </div>
+    `;
+
+    document.querySelector('.modal').append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    }, 4000);
+  }
+
+  //Do postData() for each form
+  forms.forEach(item => {
+    postData(item);
+  });
 });
